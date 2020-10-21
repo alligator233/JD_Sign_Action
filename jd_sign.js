47bf74da6e452dfcde57e47d1d8fc601083473aa
alligator233 <46662899+alligator233@users.noreply.github.com>
2020-10-21 14:44:20 +0800

    Update jd_sign.js

--- jd_sign.js
+++ jd_sign.js
@@ -76,12 +76,10 @@ function sendNotificationIfNeed() {
 
   // 去除末尾的换行
   let SCKEY = push_key.replace(/[\r\n]/g,"")
-  let url = encodeURI('https://qmsg.zendee.cn:443/send/${SCKEY}?msg='+desp)
   const options ={
-    uri: url,
-    //uri:  `https://sc.ftqq.com/${SCKEY}.send`,
-    //form: { text, desp },
-    //json: true,
+    uri:  `https://sc.ftqq.com/${SCKEY}.send`,
+    form: { text, desp },
+    json: true,
     method: 'POST'
   }
 
